b9ed9ae3a6968d52c26162f87ccaeefcd4545e5d
Ben Hoskings <ben@hoskings.net>
2013-11-26 17:46:48 +1100

    Update to ruby-1.9.2-p484 for CVE-2013-4164.

--- provision.rb
+++ provision.rb
@@ -114,7 +114,7 @@ dep 'host provisioned', :host, :host_name, :ref, :env, :app_name, :app_user, :do
       end
 
       # Build ruby separately, because it changes the ruby binary for subsequent deps.
-      remote.babushka 'corkboard:ruby.src', :version => '1.9.3', :patchlevel => 'p448'
+      remote.babushka 'corkboard:ruby.src', :version => '1.9.3', :patchlevel => 'p484'
 
       # All the system-wide config for this app, like packages and user accounts.
       remote_babushka "corkboard:system provisioned", :host_name => host_name, :env => env, :app_name => app_name, :app_user => app_user, :key => keys
