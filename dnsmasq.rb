478dd2768911943028b1aa148a60cd1c42ecec54
Ben Hoskings <ben@hoskings.net>
2013-12-02 00:10:38 +1100

    Make sure dnsmasq only listens locally.

--- dnsmasq.rb
+++ dnsmasq.rb
@@ -1,8 +1,13 @@
 
 dep 'dnsmasq' do
   requires 'dnsmasq.bin'
+  def listening_locally?
+    shell('lsof -P -n -Fn -i :53').
+      split("\n").collapse(/^n/).uniq == ['127.0.0.1:53']
+  end
   met? {
-    Babushka::Renderable.new("/etc/dnsmasq.conf").from?(dependency.load_path.parent / "dnsmasq/dnsmasq.conf.erb")
+    Babushka::Renderable.new("/etc/dnsmasq.conf").from?(dependency.load_path.parent / "dnsmasq/dnsmasq.conf.erb") &&
+    listening_locally?
   }
   meet {
     render_erb "dnsmasq/dnsmasq.conf.erb", :to => "/etc/dnsmasq.conf"
