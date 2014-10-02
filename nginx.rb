243dc70b7b72746594b6b0c344fd75ed9876dca9
Ben Hoskings <ben@hoskings.net>
2014-10-02 17:42:54 +0900

    The vhost configs are named by app_name, not domain.

--- nginx.rb
+++ nginx.rb
@@ -5,9 +5,9 @@ meta :nginx do
   def nginx_pid;    nginx_prefix / 'logs/nginx.pid' end
   def cert_path;    nginx_prefix / "conf/certs" end
   def nginx_conf;   nginx_prefix / "conf/nginx.conf" end
-  def vhost_conf;   nginx_prefix / "conf/vhosts/#{domain}.conf" end
-  def vhost_common; nginx_prefix / "conf/vhosts/#{domain}.common" end
-  def vhost_link;   nginx_prefix / "conf/vhosts/on/#{domain}.conf" end
+  def vhost_conf;   nginx_prefix / "conf/vhosts/#{app_name}.conf" end
+  def vhost_common; nginx_prefix / "conf/vhosts/#{app_name}.common" end
+  def vhost_link;   nginx_prefix / "conf/vhosts/on/#{app_name}.conf" end
 
   def upstream_name
     "#{domain}.upstream"
